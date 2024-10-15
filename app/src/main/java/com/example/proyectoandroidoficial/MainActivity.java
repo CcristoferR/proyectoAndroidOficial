121899cbf0cc1d3638b1279b4eb1e6e4f4478f61
Axelito26 <axel.mldx@gmail.com>
2024-10-15 00:58:30 -0300

    Crud main realizado, con boton crear funcionando

--- app/src/main/java/com/example/proyectoandroidoficial/MainActivity.java
+++ app/src/main/java/com/example/proyectoandroidoficial/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.proyectoandroidoficial;
 
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -15,10 +17,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        // Configura el botón para abrir la nueva actividad
+        Button btnIrCrear = findViewById(R.id.btn_ir_crear);
+        btnIrCrear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openAgregarTareaActivity();
+            }
+        });
+
+        // Ajustar los insets de la vista
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
+
+    private void openAgregarTareaActivity() {
+        Intent intent = new Intent(MainActivity.this, AgregarTareaActivity.class);
+        startActivity(intent);
+    }
 }
+
+
+
+
+
