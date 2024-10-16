79a59ea268b4ebb99e498b00cfefb1311f2c26f9
VceNr <vicenten1617@gmail.com>
2024-10-16 09:59:53 -0300

    modelo historial agregado

--- app/src/main/java/com/example/proyectoandroidoficial/MainActivity.java
+++ app/src/main/java/com/example/proyectoandroidoficial/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.proyectoandroidoficial;
 
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -15,10 +17,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+
+
+        Button btnIrEliminar = findViewById(R.id.btn_ir_historial);
+        btnIrEliminar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                OpenActivityHistorial();
+            }
+        });
+
+
+
+
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    private void OpenActivityHistorial() {
+        Intent intent = new Intent(MainActivity.this, ActivityHistorial.class);
+        startActivity(intent);
     }
 }
