e87fad05d654b24e9993ca01b8e9c4d1c1c93612
CcristoferR <cristoferalvaradotorralbo0@gmail.com>
2024-10-24 00:11:42 -0300

    integracion de las interfaces

---
+++ app/src/main/java/com/proyectoandroid/pruebafirebase/ActivityHistorial.java
@@ -0,0 +1,13 @@
+package com.proyectoandroid.pruebafirebase;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ActivityHistorial extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_historial); // Asegúrate de que este archivo XML exista
+    }
+}
--- app/src/main/java/com/proyectoandroid/pruebafirebase/HomeActivity.java
+++ app/src/main/java/com/proyectoandroid/pruebafirebase/HomeActivity.java
@@ -1,6 +1,9 @@
 package com.proyectoandroid.pruebafirebase;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class HomeActivity extends AppCompatActivity {
@@ -8,6 +11,46 @@ public class HomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home); // Asegúrate de que este archivo XML exista
+        setContentView(R.layout.activity_home);
+
+
+
+
+        Button btnCrearTarea = findViewById(R.id.btn_ir_crear);
+        Button btnEditarTarea = findViewById(R.id.btn_ir_editar);
+        Button btnEliminarTarea = findViewById(R.id.btn_ir_eliminar);
+        Button btnVerHistorial = findViewById(R.id.btn_ir_historial);
+
+        btnCrearTarea.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, com.proyectoandroid.pruebafirebase.AgregarTareaActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnEditarTarea.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, EditarTareaActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnEliminarTarea.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, EliminarTareaActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnVerHistorial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, ActivityHistorial.class);
+                startActivity(intent);
+            }
+        });
     }
 }
